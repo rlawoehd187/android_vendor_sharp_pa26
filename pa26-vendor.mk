de9510750c776bd5741d6743056c83abfc9e9b74
rlawoehd187 <rlawoehd187@naver.com>
2020-12-13 00:21:16 +0900

    pa26: Update Ril blobs.

--- pa26-vendor.mk
+++ pa26-vendor.mk
@@ -23,7 +23,6 @@ PRODUCT_COPY_FILES += \
     vendor/sharp/pa26/proprietary/bin/netmgrd:system/bin/netmgrd \
     vendor/sharp/pa26/proprietary/bin/qmuxd:system/bin/qmuxd \
     vendor/sharp/pa26/proprietary/bin/qseecomd:system/bin/qseecomd \
-    vendor/sharp/pa26/proprietary/bin/radish:system/bin/radish \
     vendor/sharp/pa26/proprietary/bin/rfs_access:system/bin/rfs_access \
     vendor/sharp/pa26/proprietary/bin/rmt_storage:system/bin/rmt_storage \
     vendor/sharp/pa26/proprietary/bin/sensors.qcom:system/bin/sensors.qcom \
